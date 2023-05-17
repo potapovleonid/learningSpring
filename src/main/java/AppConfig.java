7e5b704855e8643085ef6ae6defe0c551eac77ff
Leonid <kissmykrabeskill@gmail.com>
2023-05-17 23:52:45 +0500

    test create camera without xml with annotations

---
+++ src/main/java/AppConfig.java
@@ -0,0 +1,17 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean(name = "cameraRoll")
+    public CameraRoll cameraRoll(){
+        return new ColorCameraRoll();
+    }
+
+    @Bean(name = "camera")
+    public Camera camera(CameraRoll cameraRoll){
+        return new CameraImpl(cameraRoll);
+    }
+
+}
