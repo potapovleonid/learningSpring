0e0c4937c1f4290766006cda3ece50a7e87eace1
Leonid <kissmykrabeskill@gmail.com>
2023-05-18 00:01:59 +0500

    added component scan

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -1,13 +1,10 @@
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan("\\")
 public class AppConfig {
-
-    @Bean(name = "cameraRoll")
-    public CameraRoll cameraRoll(){
-        return new ColorCameraRoll();
-    }
 
     @Bean(name = "camera")
     public Camera camera(CameraRoll cameraRoll){
